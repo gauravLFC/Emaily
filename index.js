4dc3739b845373fd4a23cd64c4d9f1b9bc0e3ef9
Gaurav Sharma <gauravlfc.93@gmail.com>
2019-06-09 08:05:48 +0530

    minor changes in index.js

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ authRoutes(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'));
+  app.use(express.static('client/build'));
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html '));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html '));
   });
 }
 
